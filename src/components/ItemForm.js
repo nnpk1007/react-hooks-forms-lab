02a98d8e19d93f8355229a6f57faf52a7f5de03c
Ngoc Nguyen <nnpk0410@gmail.com>
2023-06-02 18:30:32 -0500

    complete lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit}) {
+  const [itemName, setItemName] = useState("")
+  const [category, setCategory] = useState("Produce")
+
+  function handleNameChange(event) {
+    setItemName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name:itemName,
+      category: category
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" 
+        name="name" 
+        value={itemName} 
+        onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" 
+        value={category} 
+        onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,26 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items
+    // category
+    .filter(
+      (item) => selectedCategory === "All" || item.category === selectedCategory
+    )
+    // search term
+    .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} search={search} onSearchChange={setSearch}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -30,3 +33,42 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+// import React, { useState } from "react";
+// import ItemForm from "./ItemForm";
+// import Filter from "./Filter";
+// import Item from "./Item";
+
+// function ShoppingList({ items, onItemFormSubmit }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+//   const [search, setSelectedSearch] = useState("");
+
+//   function handleCategoryChange(event) {
+//     setSelectedCategory(event.target.value);
+//   }
+//   const itemsToDisplay = items
+//     // category
+//     .filter(
+//       (item) => selectedCategory === "All" || item.category === selectedCategory
+//     )
+//     // search term
+//     .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
+//   return (
+//     <div className="ShoppingList">
+//       <ItemForm onItemFormSubmit={onItemFormSubmit} />
+//       <Filter
+//         search={search}
+//         onSearchChange={setSelectedSearch}
+//         onCategoryChange={handleCategoryChange}
+//       />
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
